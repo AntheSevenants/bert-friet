16d57a2930c2295e503df05aa3f18fa9e1725539
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2023-11-13 11:20:26 +0100

    Add zip code join

--- bert-friet.R
+++ bert-friet.R
@@ -11,6 +11,8 @@ df <- subset(df, select = -c(province))
 # Remove non-Belgian attestations
 # (sorry Dutchies)
 df <- df[df$country == "Belgium",]
+# Remove all attestations with empty zip codes
+df <- df[df$zip != "",]
 
 df$FRITUUR <- as.factor(df$FRITUUR)
 df$FRIGO <- as.factor(df$FRIGO)
@@ -18,3 +20,11 @@ df$CURRYWORST <- as.factor(df$CURRYWORST)
 df$SPEL.REDUCTIE <- as.factor(df$SPEL.REDUCTIE)
 df$GRIZZLY <- as.factor(df$GRIZZLY)
 
+# Read the postal codes dataset
+df_zip <- read.csv("data/zipcode-belgium.csv",
+                   header=FALSE,
+                   col.names = c("zip", "name", "lat", "long"))
+df_zip = df_zip[order(df_zip[,'zip'],-df_zip[,'zip']),]
+df_zip = df_zip[!duplicated(df_zip$zip),]
+
+df <- merge(x = df, y = df_zip, by.x="zip.code", by.y="zip", all.x=TRUE)
